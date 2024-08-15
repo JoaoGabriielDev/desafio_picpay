0e25b3f2d921b8601b638e6ca0de8824c64c61ae
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-08-15 11:05:22 -0300

    UserRepository Criado

---
+++ src/main/java/br/com/picpay/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.picpay.repositories;
+
+import br.com.picpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+   Optional<User> findUserByDocument(String document);
+
+   Optional<User> findUserById(Long id);
+
+
+}
